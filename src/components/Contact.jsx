18e7c9446410962dd1bffeec73fd8f1768b86cad
Tommmychinn <tommychinn1@gmail.com>
2020-04-08 10:42:37 +0100

    5th quete react

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Chat.css";
 import PropTypes from "prop-types";
 
-function Contact(props) {
-  return (
-    <div className="Contact">
-      <img className="avatar" src={props.image} alt="Peter Palmer" />
-      <div>
-        <p className="name">{props.name}</p>
-        <div className="status">
-          <div className={props.online ? "status-online" : "status-offline"} />
-          <p className="status-text">online</p>
-        </div>
-      </div>
-    </div>
-  );
+class Contact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      online: props.online
+    }
+  }
+  
+  render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.image} alt="Peter Palmer" />
+                <div>
+                    <p className="name">{this.props.name}</p>
+                    <div onClick={()=>{
+                      this.setState({online: true})
+                    }} className="status">
+                        <div
+                            className={
+                                this.state.online
+                                    ? "status-online"
+                                    : "status-offline"
+                            }
+                        />
+                        <p className="status-text">{this.state.online? "online" : "offline"}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 Contact.propTypes = {
-  image: PropTypes.string,
-  name: PropTypes.string,
-  online: PropTypes.bool
+    image: PropTypes.string,
+    name: PropTypes.string,
+    online: PropTypes.bool,
 };
 
 export default Contact;
